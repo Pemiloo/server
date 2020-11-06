912fd0cc9714b08137e4f994b378cbc507260819
aboutindracom <me@aboutindra.com>
2020-11-06 20:42:19 +0700

    Test automation

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Yeaay.js!</a>
+          Welcome to <a href="https://nextjs.org">Okeee.js!</a>
         </h1>
 
         <p className={styles.description}>

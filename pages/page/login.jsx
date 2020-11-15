9c512f0d6aefe52c86fd1f2bc6882ebf80e20610
dev-zam <zamsyh.work@gmail.com>
2020-11-15 14:51:27 +0700

    Initial commit

---
+++ pages/page/login.jsx
@@ -0,0 +1,26 @@
+import Head from 'next/head';
+import React from 'react';
+import style from '../../styles/page/login.module.css';
+
+const Login = () => {
+    return(
+        <div className={style.body}>
+            <Head>
+                <title>Pemilo - Login Page</title>
+                <meta charset="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <link rel="icon" href="/pemilo.svg" />
+            </Head>
+        
+
+        <div className={style.container}>
+            
+        </div>
+            
+
+
+        </div>
+    )
+}
+
+export default Login;

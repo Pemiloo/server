ff69debcc317bf346bb369010130b445699a25e2
amardito <amardito37@gmail.com>
2020-11-17 01:06:09 +0700

    patch on nav bar

--- pages/components/nav.jsx
+++ pages/components/nav.jsx
@@ -1,29 +1,33 @@
 import s from '../../styles/components/nav.module.css'
 
-const nav = () => {
+const Nav = () => {
   return(
-    <div className={s.container}>
-      <div className={s.logo}>
-        <img src="/logo.svg" alt="logo pemilo"/>
-      </div>
-      <div className={s.navMenu}>
+    <div className={s.containerFluid}>
+      <div className={s.container}>
 
-        <div className={s.menuList}>
-          <div className={s.menu}>
-            <span>Room</span>
-          </div>
-          <div className={s.menu}>
-            <img src="/icon/plus.svg"/>
-          </div>
+        <div className={s.logo}>
+          <img src="/logo.svg" alt="logo pemilo"/>
         </div>
+        <div className={s.navMenu}>
 
-        <div className={s.profile}>
-          <span>Name Profile Organization</span>
-          <img src="/pemilo.svg" alt="profile"></img>
+          <div className={s.menuList}>
+            <div className={s.menu}>
+              <span>Room</span>
+            </div>
+            <div className={s.menu}>
+              <img src="/icon/plus.svg" alt="icon plus"/>
+            </div>
+          </div>
+
+          <div className={s.profile}>
+            <span>Name Profile Organization</span>
+            <img src="/pemilo.svg" alt="profile"></img>
+          </div>
+
         </div>
 
       </div>
     </div>
   )
 }
-export default nav
+export default Nav

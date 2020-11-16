8eea6e2829baaba7679b757c8a63d604ae67a412
dev-zam <zamsyh.work@gmail.com>
2020-11-16 12:35:11 +0700

    Add page admin listroom

---
+++ pages/page/admin/list-room.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Head from 'next/head';
+import Nav from '../../components/nav';
+
+const listRoom = () => {
+    return(
+        <div>
+           <Nav />
+        </div>
+        
+    )
+}
+
+export default listRoom;

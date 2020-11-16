5a362d68248b9157cfb91702e51c533dfced9bd5
dev-zam <zamsyh.work@gmail.com>
2020-11-16 12:06:37 +0700

    Update login page

--- pages/page/login.jsx
+++ pages/page/login.jsx
@@ -4,7 +4,7 @@ import style from '../../styles/page/login.module.css';
 
 const Login = () => {
     return(
-        <div className={style.body}>
+        <div className={style.container}>
             <Head>
                 <title>Pemilo - Login Page</title>
                 <meta charset="UTF-8" />
@@ -13,8 +13,26 @@ const Login = () => {
             </Head>
         
 
-        <div className={style.container}>
-            
+        <div className={style.card}>
+            <div className={style.card_body}>
+                <div className={style.card_info}>
+                    <span className={style.title}>Pemilo</span>
+                    <p className={style.desc}><b>Pemilo</b> is <br></br>online voting program based on website platform</p>
+                </div>
+                <div className={style.card_form}>
+                    <form method="post">
+                        <p className={style.label}>Your Email</p>
+                        <input type="email" name="email" id="email" className={style.input} placeholder="Enter your email" autoComplete="off" required="true" />
+
+                        <p className={style.label}>Your Password</p>
+                        <input type="password" name="password" id="password" className={style.input} placeholder="Enter your password" autoComplete="off" required="true" />
+
+                        <p><input type="checkbox" name="check" id="check" className={style.checkbox} required="true" /> <span className={style.t_checkbox}>I am certify that email still active </span> </p>
+
+                        <p><input type="submit" name="register" id="register" value="Register" className={style.button}/></p>
+                    </form>
+                </div>
+            </div>
         </div>
             
 

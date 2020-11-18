86d1f1cb376241084c1f9349928ad801f815dbde
amardito <amardito37@gmail.com>
2020-11-18 20:15:59 +0700

    modeling structure on detail room

---
+++ pages/page/detail-room.jsx
@@ -0,0 +1,22 @@
+import s from '../../styles/page/detail-room.module.css'
+import Nav from '../components/nav'
+
+const detailRoom = () => {
+  return(
+    <>
+      <Nav></Nav>
+      <div>
+
+        <div>
+          <div></div>
+          <div></div>
+        </div>
+        
+        <div></div>
+
+      </div>
+    </>
+  )
+}
+
+export default detailRoom;

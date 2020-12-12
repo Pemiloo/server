922e8237ff0d49c5e0da6a7dfe59106ae7f4c818
amardito <amardito37@gmail.com>
2020-12-12 07:19:27 +0700

    change url

--- api/vote.js
+++ api/vote.js
@@ -1,6 +1,6 @@
 import { Http } from "../lib";
 
-const http = new Http('http://34.101.95.115:3400');
+const http = new Http('https://pemilo.id/v1/vote');
 
 export async function getLen(room = "", id = ""){
   const data = await http.post("/total", {codeRoom:room, idCandidate:id});
